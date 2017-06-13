53e30f03bec6e117d0c8e8e18ee3a31e57dd30e2
nyamwaya <alecksonnyamwaya@gmail.com>
2017-06-13 13:11:46 -0500

    created the repository

---
+++ app/src/main/java/nyamwaya/com/a75f/repository/AirQualityRepository.java
@@ -0,0 +1,13 @@
+package nyamwaya.com.a75f.repository;
+
+import android.arch.lifecycle.LiveData;
+
+import nyamwaya.com.a75f.util.ApiResponse;
+
+/**
+ * Created by Aleckson on 6/13/2017.
+ */
+
+public interface AirQualityRepository {
+    LiveData<ApiResponse> getAirQualityData(String latitude, String longitude, String apikey);
+}
---
+++ app/src/main/java/nyamwaya/com/a75f/repository/AirQualityRepositoryImpl.java
@@ -0,0 +1,59 @@
+package nyamwaya.com.a75f.repository;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+
+import nyamwaya.com.a75f.models.AirQualityModel;
+import nyamwaya.com.a75f.network.AirQualityApiService;
+import nyamwaya.com.a75f.util.ApiResponse;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Aleckson on 6/13/2017.
+ *
+ * The reason for this class is to abstract the communication
+ * between the data source (local database or API callas)
+ * This class will use the api service to fetch air quality data from our API
+ *
+ * It will return the data as a LiveData.
+ */
+
+public class AirQualityRepositoryImpl {
+
+    private static final String BASEURL = "https://api.breezometer.com";
+    private AirQualityApiService mApiService;
+
+    public AirQualityRepositoryImpl() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BASEURL)
+                .build();
+        mApiService = retrofit.create(AirQualityApiService.class);
+    }
+
+    public LiveData<ApiResponse> getAirQualityData (String latitude, String longitude, String apikey) {
+        final MutableLiveData<ApiResponse> liveData = new MutableLiveData<>();
+        Call<AirQualityModel> call = mApiService.getAirQuality(latitude, longitude, apikey);
+
+        call.enqueue(new Callback<AirQualityModel>() {
+            @Override
+            public void onResponse(Call<AirQualityModel> call, Response<AirQualityModel> response) {
+                liveData.setValue(new ApiResponse(response.body()));
+            }
+
+            @Override
+            public void onFailure(Call<AirQualityModel> call, Throwable t) {
+
+                liveData.setValue(new ApiResponse(t));
+            }
+        });
+        return liveData;
+    }
+
+
+
+}
