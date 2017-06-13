9bd152b63bcc1146327e71351c0728bb2de43d97
nyamwaya <alecksonnyamwaya@gmail.com>
2017-06-13 12:54:24 -0500

    created retrofit api service

--- app/src/main/java/nyamwaya/com/a75f/network/AirQualityApiService.java
+++ app/src/main/java/nyamwaya/com/a75f/network/AirQualityApiService.java
@@ -1,8 +1,18 @@
 package nyamwaya.com.a75f.network;
 
+import nyamwaya.com.a75f.models.AirQualityModel;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 /**
  * Created by Aleckson on 6/13/2017.
  */
 
 public interface AirQualityApiService {
+    @GET("/baqi/")
+    Call<AirQualityModel> getAirQuality(
+            @Query("lat") String latitude,
+            @Query("lon") String longitude,
+            @Query("key") String apikey );
 }
